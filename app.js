32e57028f63cb0a5b47320449034cb6543b19fa0
Carlos Gomez <carlosgomz1331@gmail.com>
2022-08-20 12:58:55 -0400

    add profile card generator

---
+++ app.js
@@ -0,0 +1,71 @@
+    function render(variables = {}) {
+        console.log("These are the current variables: ", variables);
+        let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+        if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+        document.querySelector("#widget_content").innerHTML = `<div class="widget">
+                  ${cover}
+                <img src="${variables.avatarURL}" class="photo" />
+                <h1>${variables.name ? variables.name : "Name"} ${
+          variables.lastname ? variables.lastname : "Last Name"
+        }</h1>
+                <h2>${variables.role ? variables.role : "Role"}</h2>
+                <h3>${variables.city ? variables.city : "City"},${
+          variables.country ? variables.country : "Country"
+        }</h3>
+                <ul class="${variables.socialMediaPosition}">
+                  <li><a target="_blank" href="https://twitter.com/${
+                    variables.twitter ? variables.twitter : "Twitter User"
+                  }"><i class="fab fa-twitter"></i></a></li>
+                  <li><a target="_blank" href="https://github.com/${
+                    variables.github ? variables.github : "Github User"
+                  }"><i class="fab fa-github"></i></a></li>
+                  <li><a target="_blank" href="https://linkedin.com/${
+                    variables.linkedin ? variables.linkedin : "Linked In User"
+                  }"><i class="fab fa-linkedin"></i></a></li>
+                  <li><a target="_blank" href="https://instagram.com/${
+                    variables.instagram ? variables.instagram : "Instagram User"
+                  }"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+              </div>
+          `;
+      }
+     
+      window.onload = function() {
+        window.variables = {
+          includeCover: true,
+          
+          background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+          
+          avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+          
+          socialMediaPosition: "position-right",
+          twitter: null,
+          github: "carlossgomz",
+          linkedin: null,
+          instagram: null,
+          name: null,
+          lastname: null,
+          role: null,
+          country: null,
+          city: null
+        };
+        render(window.variables);
+      
+        document.querySelectorAll(".picker").forEach(function(elm) {
+          elm.addEventListener("change", function(e) {
+            
+            const attribute = e.target.getAttribute("for"); 
+            let values = {};
+            values[attribute] =
+              this.value == "" || this.value == "null"
+                ? null
+                : this.value == "true"
+                ? true
+                : this.value == "false"
+                ? false
+                : this.value;
+            render(Object.assign(window.variables, values)); 
+          });
+        });
+      };
